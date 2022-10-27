e9797c3d9f6b389f669712cfffba9fbea457d604
claudiovr <claudio14.ribeiro@gmail.com>
2022-10-26 23:59:30 -0300

    segunda versao

--- grid2.py
+++ grid2.py
@@ -7,10 +7,9 @@ app = Flask(__name__)
 
 @app.route("/") 
 def homepage():
-    return render_template("mapaW3-v6-Alt4.html")
+    return render_template("mapaW3-v6-Alt5.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
 
-    #servidor do heroku
-    
