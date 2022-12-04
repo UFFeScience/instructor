48b8d8982d00d90c5ae7bf6f170194809c0f0dbe
claudiovr <claudio14.ribeiro@gmail.com>
2022-12-04 12:00:03 -0300

    ver 04Dez22

--- grid.py
+++ grid.py
@@ -1,22 +1,49 @@
 #import pythonCode.py
-#import json
+import json
 import flask
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 import pandas as pd
 
+def teste():
+    print ("teste do python")
+
 app = Flask(__name__)
 
-@app.route("/") 
+@app.route('/') 
 def homepage():
-    valor = [[32.52188,-80.11495],[32.36890,-80.29070],[32.11337,-80.49666],[32.27142, -80.20832]]
-    return render_template("mapaW3-v6-Alt4.html", valor = valor)
-    #return render_template("mapaW3-v6-Alt4.html")
 
-#@app.route("/carregaArq")
-#def carregaArq():
+    return render_template("mapaW3-v6-Alt4.html")
+
+@app.route('/create_file', methods=['POST'])
+def create_file():
+    if request.method == 'POST':
+        #print (request.form)
+        with open(f"{request.form.get('name')}.txt", "w") as f:
+            f.write('FILE CREATED AND SUCCESSFULL POST REQUEST!')
+        return ('', 204)
+
+@app.route('/openFileAndFilteAOI', methods=['POST'])
+def openFileAndFilterAOI():
+    llon = ulon = llat = ulat = 0.0
+
+    if request.method == 'POST':
+       ais_df = pd.read_csv('F:/AIS_2020_01_10.csv')
+       llon = float(request.form.get('llon', type=float))
+       ulon = float(request.form.get('ulon', type=float))
+       llat = float(request.form.get('llat', type=float))
+       ulat = float(request.form.get('ulat', type=float))
+       print ("lats e longs = ", llon)
+       print ("Tam array ais antes = ",ais_df.shape)
+       ######### filtro
+       ais_df = ais_df[(ais_df['LON'] > llon)]
+       ais_df = ais_df[(ais_df['LON'] < ulon)]
+       ais_df = ais_df[(ais_df['LAT'] > llat)]
+       ais_df = ais_df[(ais_df['LAT'] < ulat)]
+       print ("Tam array ais depois = ",ais_df.shape)
+       print(ais_df.head(3))
+       return (str(ulat), 204)
     
-#    return render_template("mapaW3-v6-Alt5.html")
 
 
 if __name__ == "__main__":
