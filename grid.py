dc0911bf7b83af136487cb503b792afc11cbb063
ribeirocv <claudio14.ribeiro@gmail.com>
2023-02-27 23:42:14 -0300

    deploy 27fev23 show traj

--- grid.py
+++ grid.py
@@ -1,14 +1,18 @@
 #import pythonCode.py
 import json
 import flask
-from flask import Flask, Response, render_template, request, redirect, url_for, jsonify
+from flask import Flask, Response, render_template, request, redirect, url_for, jsonify,send_from_directory,send_file
 from flask_cors import CORS, cross_origin # permitir back receber json do front
 
 import pandas as pd
 import tkinter as tk
 from tkinter import filedialog as fd
+import os
+import shutil
+import io
 
-#locations_dfBuffer = ""
+
+
 
 def teste():
     print ("teste do python")
@@ -31,11 +35,10 @@ def create_file():
 @app.route('/loadFileAOI', methods=['GET'])
 def loadFileAOI():
     
-    global locations_dfBuffer
     locations_df = pd.read_csv(selectNameFile())
-    #locations_df = pd.read_csv('F:/LatLongAOI.csv')
     locations_dfBuffer = locations_df.copy() # novo 05fev
     locations_df_json = locations_df.to_json(orient='values')
+    
     resposta = jsonify(locations_df_json)
     resposta.headers.add("Access-Control-Allow-Origin", "*")
     return resposta
@@ -54,18 +57,20 @@ def selectDatasetFile():
     resposta.headers.add("Access-Control-Allow-Origin", "*")
     return resposta
 
-@app.route('/openFileAndFilteAOI', methods=['POST'])
+@app.route('/openFileAndFilterAOI', methods=['POST'])
 def openFileAndFilterAOI():
+    #global locations_dfBuffer
     llon = ulon = llat = ulat = 0.0
 
     if request.method == 'POST':
        
-       #ais_df = pd.read_csv('F:/AIS_2020_01_10.csv')
        dados = request.get_json()
-       
        ais_df = pd.read_csv(dados[4]); #novo
-             
+       locations_dfBuffer = pd.read_csv(io.StringIO(dados[5]), sep=",")  #novo 23fev
+       
+       print ("print dados locations_dfBuffer inicio da funcao",locations_dfBuffer)
        print("variável do post = ", dados)
+       
        ais_df.dropna(subset=['LAT', 'LON', 'MMSI'], inplace=True) #novo
        
        llat = float(dados[0])
@@ -73,7 +78,6 @@ def openFileAndFilterAOI():
        llon = float(dados[2])
        ulon = float(dados[3])
 
-       #print ("lats e longs = ", llon)
        print ("Tam array ais antes exclusao fora do Grid = ",ais_df.shape)
        ######### filtro
        ais_df = ais_df[(ais_df['LON'] > llon)]
@@ -95,11 +99,11 @@ def openFileAndFilterAOI():
 
        for i in range(0, len(ais_df)):
             
-        lat = ais_df["LAT"][i]
-        lon = ais_df["LON"][i]
-        b_pontoInsidAOI = isPointInPolygon(lat, lon, locations_dfBuffer)
-        if b_pontoInsidAOI:
-            ais_df.loc[i,"insideAOI"]  = True
+            lat = ais_df["LAT"][i]
+            lon = ais_df["LON"][i]
+            b_pontoInsidAOI = isPointInPolygon(lat, lon, locations_dfBuffer)
+            if b_pontoInsidAOI:
+                ais_df.loc[i,"insideAOI"]  = True
 
        ais_df = ais_df[(ais_df['insideAOI'] == True)]
        
@@ -110,30 +114,60 @@ def openFileAndFilterAOI():
        ais_df = ais_df.reset_index(drop=True) # novo
        ais_df['speedBIN']  = ""  #novo
        ais_df['courseBIN'] = ""  #novo
-    # novo 12Jan
+       ais_df['TrajID'] = ""  #novo 
+
        vSpeed = 0
        vCourse = 0
 
+       #########
+       ais_df.to_csv("d:ais_orig.csv", index=False)
+       MMSI_previous = ais_df["MMSI"][0]
+       id_TrajID = 1 
+       
        for i in range(0, len(ais_df)):
-        #for i in range(0, 10):
-        vSpeed  = ais_df["SOG"][i]
-        vCourse = ais_df["COG"][i]
-        speed, course = geracaoBINs(vSpeed, vCourse)
-        ais_df.loc[i,"speedBIN"]  = speed
-        ais_df.loc[i,"courseBIN"] = course
-        #ais_df["speedBIN"][i]  = speed
-        #ais_df["courseBIN"][i] = course
-    # fim novo 12 Jan
+            
+            vSpeed  = ais_df["SOG"][i]
+            vCourse = ais_df["COG"][i]
+            ########
+            MMSI_actual = ais_df['MMSI'][i]
+            #if vSpeed < 1:
+            #    index_Traj = index_Traj + 1
+                        
+            if MMSI_previous != MMSI_actual:
+                MMSI_previous = MMSI_actual
+                id_TrajID = id_TrajID + 1
+
+            speed, course = geracaoBINs(vSpeed, vCourse)
+            ais_df.loc[i,"speedBIN"]  = speed
+            ais_df.loc[i,"courseBIN"] = course
+            ais_df.loc[i,"TrajID"] = id_TrajID
+            
+       ais_df.to_csv("d:ais_trajID.csv", index=False)
 
        print ("Tam array ais depois = ",ais_df.shape)
        print(ais_df.head(10))
        #ais_df_em_json = [x.to_json() for x in ais_df]
        #resposta = jsonify(ais_df_em_json)
        ais_df_json = ais_df.to_json(orient='values')
-       resposta = jsonify(ais_df_json)
+       resposta = jsonify(ais_df_json, id_TrajID)
        resposta.headers.add("Access-Control-Allow-Origin", "*")
        return resposta #("", 204)
     
+
+def selectNameDir():
+    root = tk.Tk()
+    root.geometry("500x400") # not working
+    root.wm_attributes('-topmost', 1)   
+    root.lift()     # to work with others OS which are not windows
+    root.withdraw()
+   
+    dirPath = fd.askdirectory(parent=root, title='Select a directory')
+    #dirPath = fd.askopenfilename(parent=root, title='Select a file', filetypes=[("CSV files", ".csv")])
+    print(dirPath)
+    root.destroy()
+ 
+    return dirPath
+
 def selectNameFile():
     root = tk.Tk()
     root.geometry("500x400") # not working
@@ -146,6 +180,113 @@ def selectNameFile():
     root.destroy()
  
     return filepath
+
+@app.route('/checkLoginName', methods=['POST'])
+def checkLoginName():
+        
+    global fileNameExpert
+    global loginName
+    
+    loginName = request.get_json()
+    print("linha 152 loginName = ", loginName)
+    fileNameExpert = "none"
+    expertsID_df = pd.read_csv("static/expertFiles/expertsID.csv")
+    print("arq expertsID = ", expertsID_df.head(5))
+    for i in range(0, len(expertsID_df)):
+        nameLoginFileCSV = expertsID_df["name"][i]
+        if loginName == nameLoginFileCSV:
+            print ("linha 160 nome = ", nameLoginFileCSV)
+            fileNameExpert = expertsID_df["filename"][i]
+                
+    print("filename = ", fileNameExpert)
+    resposta = jsonify(fileNameExpert)
+    #resposta = fileNameExpert
+    resposta.headers.add("Access-Control-Allow-Origin", "*")
+    return resposta
+
+@app.route('/loadExpertFile', methods=['GET'])
+def loadExpertFile():
+    global expert_df
+    global expert_full_df
+    expertCSV_File = "static/expertFiles/" + fileNameExpert
+    expert_df = pd.read_csv(expertCSV_File)
+    expert_full_df = pd.read_csv("static/expertFiles/" + "expert_full.csv") # novo 22fev
+    print(expertCSV_File)
+    print(expert_df)
+    resposta = jsonify(expertCSV_File)
+    resposta.headers.add("Access-Control-Allow-Origin", "*")
+    return resposta
+
+@app.route('/downloadClassification',  methods=["GET"])
+def downloadClassification():
+    
+    #arquivos = []
+    #for nome_do_arquivo in os.listdir(strDirectory):
+     #   endereco_do_arquivo = os.path.join(strDirectory, nome_do_arquivo)
+
+      #  if(os.path.isfile(endereco_do_arquivo)):
+       #     arquivos.append(nome_do_arquivo)
+
+    #print("lista de arquivos = ", arquivos)
+    strDirName = selectNameDir()
+    src1 = 'static/expertFiles/' + fileNameExpert
+    src2 = 'static/expertFiles/' + 'expert_full.csv'
+    destination1 = strDirName + '/' + fileNameExpert
+    destination2 = strDirName + '/' + 'expert_full.csv'
+
+    shutil.copyfile(src1, destination1)
+    shutil.copyfile(src2, destination2)
+    
+    resposta = jsonify(src1)# response useless
+    resposta.headers.add("Access-Control-Allow-Origin", "*")
+    return resposta
+
+@app.route('/saveClassification', methods=['POST'])
+def saveClassification():
+    expertID = loginName
+    dados = request.get_json()
+    trajetoriaID = dados[0]
+    clusteringSelection = dados[1]
+    normal = dados[2]
+    confiabilitySelection = dados[3]
+    data = dados[4]
+    hora = dados[5]
+    expertCSV_File = "static/expertFiles/" + fileNameExpert
+    expertCSC_FullFile = "static/expertFiles/" + "expert_full.csv"
+  
+    index = len(expert_df)
+    index_full = len(expert_full_df)
+    print("tamanho expert_df = ", index)
+
+    expert_df.loc[index,"Trajetoria"]  = trajetoriaID
+    expert_full_df.loc[index_full,"Trajetoria"]  = trajetoriaID
+
+    expert_df.loc[index,"Expert"]  = expertID
+    expert_full_df.loc[index_full,"Expert"]  = expertID
+
+    expert_df.loc[index,"Clustering"]  = clusteringSelection
+    expert_full_df.loc[index_full,"Clustering"]  = clusteringSelection
+
+    expert_df.loc[index,"Normal"]  = normal
+    expert_full_df.loc[index_full,"Normal"]  = normal
+
+    expert_df.loc[index,"Conf"]  = confiabilitySelection
+    expert_full_df.loc[index_full,"Conf"]  = confiabilitySelection
+
+    expert_df.loc[index,"Date"]  = data
+    expert_full_df.loc[index_full,"Date"]  = data
+
+    expert_df.loc[index,"Time"]  = hora
+    expert_full_df.loc[index_full,"Time"]  = hora
+
+    print("expert_df = ", expert_df)
+    expert_df.to_csv(expertCSV_File, index=False)
+    expert_full_df.to_csv(expertCSC_FullFile, index=False)
+    
+    resposta = jsonify(expertCSV_File)
+    resposta.headers.add("Access-Control-Allow-Origin", "*")
+    return resposta
+
 
 def geracaoBINs(speed, course):
     speedBIN = ""
